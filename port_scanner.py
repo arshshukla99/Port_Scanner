c65b4561c73b732169755c07efcb8f1e2a81ace4
Arsh <arshshukla2487@gmail.com>
2026-05-21 15:17:19 +0530

    Updated port_scanner.py to accept command line arguments
    Updated Port Scanner to work on command line arguments and have usage format.

--- port_scanner.py
+++ port_scanner.py
@@ -1,47 +1,52 @@
+#!/usr/bin/python3
+
+import sys
+import time
 import socket
 import threading
-import time
-import sys
 
-print("-"*100)
+print('-'*100)
 print(" "*35,"Simple Port Scanner")
 print("-"*100)
 
-start_time = time.time()
+usage = " Give arguments in the Following Format : \n\npython3 port_scanner.py TARGET_IP START_PORT END_PORT"
 
-ip = input("Enter your target IP Address: ")
-start_port = int(input("Enter the Starting Port Number: "))
-end_port = int(input("Enter the End Port Number : "))
-
-try:
-    target_ip = socket.gethostbyname(ip)
-    
-except socket.gaierror:
-    print("Invalid Ip :/")
+if len(sys.argv) != 4 :
+    print(usage)
     sys.exit()
 
-def scan_tcp(port):
+try :
+    target = socket.gethostbyname(sys.argv[1])
+except socket.gaierror:
+    print("Ip isn't Correct !")
+
+start_time = time.time()
+
+start_port = int(sys.argv[2])
+end_port = int(sys.argv[3])
+
+def port_scan(port):
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     sock.settimeout(1)
-    con = sock.connect_ex((target_ip, port))
-    if not con :
-        try:
+    con = sock.connect_ex((target,port))
+    if not con:
+        try :
             service = socket.getservbyport(port)
         except OSError:
-            service = "Unknown"
+            print("Unknown Service")
         print(f"Port {port} is OPEN : {service}")
-    sock.close()
 
 threads = []
 
-for port in range (start_port, end_port + 1) :
-    thread = threading.Thread(target = scan_tcp, args= (port,))
-    threads.append(thread)
+for port in range(start_port,end_port+1):
+    thread = threading.Thread(target=port_scan,args=(port,))
     thread.start()
+    threads.append(thread)
 
 for thread in threads:
     thread.join()
 
-end_time = time.time()
+print()
 
-print("Total Time Taken :",(end_time - start_time),"sec")
+end_time = time.time()   
+print("Scan Performed in :",(end_time - start_time))
