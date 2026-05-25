5a3f71a674ec121f4d951ba5846cbd825a8825bc
Arsh <arshshukla2487@gmail.com>
2026-05-25 23:09:32 +0530

    Enhance code readability with comments
    Added comments for better understanding of the code.

--- port_scanner.py
+++ port_scanner.py
@@ -5,16 +5,20 @@ import time
 import socket
 import threading
 
+#Adds graphics Initially
+
 print('-'*100)
 print(" "*35,"Simple Port Scanner")
 print("-"*100)
 
 usage = " Give arguments in the Following Format : \n\npython3 port_scanner.py TARGET_IP START_PORT END_PORT"
 
+#Checks if there are 4 arguments given by the user or not
 if len(sys.argv) != 4 :
     print(usage)
     sys.exit()
 
+#try to get ip between given arguments and checks for errors in ip
 try :
     target = socket.gethostbyname(sys.argv[1])
 except socket.gaierror:
@@ -25,6 +29,7 @@ start_time = time.time()
 start_port = int(sys.argv[2])
 end_port = int(sys.argv[3])
 
+#searches for open ports and service on each port
 def port_scan(port):
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     sock.settimeout(1)
@@ -39,11 +44,13 @@ def port_scan(port):
 
 threads = []
 
+#threads.start() creates a thread for each port scan and store each thread created in a threads list
 for port in range(start_port,end_port+1):
     thread = threading.Thread(target=port_scan,args=(port,))
     thread.start()
     threads.append(thread)
 
+#thread.join() waits for each thread to finish then end the program
 for thread in threads:
     thread.join()
 
