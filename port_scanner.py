5163f18662a5982108ea5792d0168405ce59da23
Arsh <arshshukla2487@gmail.com>
2026-05-29 11:59:13 +0530

    Refactor port scanner to use argparse for options
    Updated Port Scanner to version 1.1. Added arguments using argparse library in python.

--- port_scanner.py
+++ port_scanner.py
@@ -8,43 +8,58 @@ import socket
 import argparse
 import threading
 
-#Adds graphics Initially
-
 print('-'*100)
 print(" "*35,"Simple Port Scanner")
 print("-"*100)
 
 usage = " Give arguments in the Following Format : \n\npython3 port_scanner.py TARGET_IP START_PORT END_PORT"
 
-#Checks if there are 4 arguments given by the user or not
-if len(sys.argv) != 4 :
-    print(usage)
-    sys.exit()
+#Created Parser Obejct
 
-#try to get ip between given arguments and checks for errors in ip
+parser = argparse.ArgumentParser(description="Python Based Fast Port Scanner", usage="%(prog)s -t TARGET [MORE_OPTIONS] ", epilog= "Example - %(prog)s -s 1 -e 65535 -t 192.168.43.229")
+
+# Added arguments in parser for more options
+
+parser.add_argument("-s","--start", type=int, help="Start Port", default=1)
+parser.add_argument("-e","--end", type=int, help= "End Port", default= 65535)
+parser.add_argument("-t","--target", dest="target", required= True, help="Target IP or Domain")
+parser.add_argument("-th","--threads", dest="threads",type=int, help= "No. of Threads To be Used", default=500)
+#parser.add_argument("-V","--verbose", dest="verbose", action= "store_true", help= "Verbose Output")
+parser.add_argument("-v","--version", action="version", version= "%(prog)s 1.1", help= "Diplay %(prog)s Version")
+args = parser.parse_args()
+
+# Extracting IP given by User
+
 try :
-    target = socket.gethostbyname(sys.argv[1])
+    target = socket.gethostbyname(args.target)
 except socket.gaierror:
     print("Ip isn't Correct !")
+    sys.exit()
 
 start_time = time.time()
 
-start_port = int(sys.argv[2])
-end_port = int(sys.argv[3])
+#try to get ip between given arguments and checks for errors in ip
+start_port = args.start
+end_port = args.end
 
-#searches for open ports and service on each port
+if start_port < 1 or end_port > 65535 or start_port < end_port:
+    print("Invalid Ports")
+    sys.exit()
+
+#Searches for open ports and service on each port
 def port_scan(port):
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     sock.settimeout(1)
-    con = sock.connect_ex((target,port))
-    if not con:
-        try :
-            service = socket.getservbyport(port)
-        except OSError:
-            print("Unknown Service")
-        print(f"Port {port} is OPEN : {service}")
-    sock.close()
-
+    try:
+        con = sock.connect_ex((target,port))
+        if not con:
+            try :
+                service = socket.getservbyport(port)
+            except OSError:
+                service = "Unknown Service"
+            print(f"Port {port} is OPEN : {service}")
+    finally:
+        sock.close()
 threads = []
 
 #threads.start() creates a thread for each port scan and store each thread created in a threads list
